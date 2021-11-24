8fe128f42175306e0023800d3135ceb69efe5c42
Maria Jesus Bellido <mariajbellido@gmail.com>
2021-11-24 19:30:34 +0100

    initial setup with API

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	const loadCoins = async() => {
+		const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+		const data = await res.json()
+		console.log(data)
+	}
+	loadCoins()
+</script>
+
+<h1>
+	Hello World
+</h1>
+
+
+<style>
+
+</style>
