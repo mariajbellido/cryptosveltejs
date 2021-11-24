9f88401b6cec07e436da0866d7e6c679629c9943
Maria Jesus Bellido <mariajbellido@gmail.com>
2021-11-24 19:38:24 +0100

    creating table - 1

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,12 @@
 <script>
+
+	let coins = []
+
 	const loadCoins = async() => {
 		const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false')
 		const data = await res.json()
 		console.log(data)
+		coins = data;
 	}
 	loadCoins()
 </script>
@@ -10,6 +14,24 @@
 <h1>
 	Hello World
 </h1>
+<table>
+	<thead>
+		<tr>
+			<th> bla bla bla</th>
+		</tr>
+	</thead>
+	<tbody>
+	{#each coins as coin}
+		<tr>
+			<td>
+				{coin.name}
+			</td>
+		</tr>
+	{/each}
+
+	</tbody>
+
+</table>
 
 
 <style>
